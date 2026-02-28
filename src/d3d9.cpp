117dc74c12e4c89d0c8b5292a6dc0ce92ed9c6cc
Nathan Baggs <hello@nathanbaggs.com>
2026-02-28 14:39:35 +0000

    Hook some COM

--- src/d3d9.cpp
+++ src/d3d9.cpp
@@ -2,7 +2,42 @@
 
 #include <d3d9.h>
 
+#include "mandrel/hooks/com_hook.h"
 #include "mandrel/utils.h"
+
+namespace
+{
+
+template <class F>
+struct OrigFunc;
+
+template <class R, class H, class... Tail>
+struct OrigFunc<R(WINAPI *)(H, Tail...)>
+{
+    using type = R(WINAPI *)(Tail...);
+};
+
+auto com_hook = mandrel::COMHook{};
+
+::HRESULT WINAPI IDirect3D9_CreateDevice_hook(
+    ::PROC orig_func,
+    void *that,
+    ::UINT Adapter,
+    ::D3DDEVTYPE DeviceType,
+    ::HWND hFocusWindow,
+    ::DWORD BehaviorFlags,
+    ::D3DPRESENT_PARAMETERS *pPresentationParameters,
+    ::IDirect3DDevice9 **ppReturnedDeviceInterface)
+{
+    using orig_call_type = OrigFunc<decltype(&IDirect3D9_CreateDevice_hook)>::type;
+
+    mandrel::log("IDirect3D9::CreateDevice called");
+
+    return reinterpret_cast<orig_call_type>(orig_func)(
+        that, Adapter, DeviceType, hFocusWindow, BehaviorFlags, pPresentationParameters, ppReturnedDeviceInterface);
+}
+
+}
 
 extern "C"
 {
@@ -12,13 +47,18 @@ __declspec(dllexport) ::IDirect3D9 *WINAPI Direct3DCreate9(::UINT SDKVersion)
 {
     mandrel::log("Direct3DCreate9 called");
 
-    const auto d3d9 = ::LoadLibraryA("C:\\Windows\\system32\\d3d9.dll");
-    mandrel::ensure(d3d9 != nullptr, "could not load d3d9");
+    const auto d3d9_lib = ::LoadLibraryA("C:\\Windows\\system32\\d3d9.dll");
+    mandrel::ensure(d3d9_lib != nullptr, "could not load d3d9");
 
-    const auto direct_create = reinterpret_cast<decltype(&Direct3DCreate9)>(::GetProcAddress(d3d9, "Direct3DCreate9"));
+    const auto direct_create =
+        reinterpret_cast<decltype(&Direct3DCreate9)>(::GetProcAddress(d3d9_lib, "Direct3DCreate9"));
     mandrel::ensure(direct_create != NULL, "failed to get address of Direct3DCreate9");
 
-    return direct_create(SDKVersion);
+    auto *d3d9 = direct_create(SDKVersion);
+
+    com_hook.add_hook(16zu, d3d9, IDirect3D9_CreateDevice_hook);
+
+    return d3d9;
 }
 
 ::DWORD WINAPI DllMain(void *, ::DWORD fdwReason, void *)
